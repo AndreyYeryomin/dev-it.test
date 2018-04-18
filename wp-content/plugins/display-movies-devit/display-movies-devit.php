3fb94ef84182d0471631a20ffa276a090835a0c7
AndreyYeryomin <andrey.yeryomin11@gmail.com>
2018-04-19 01:04:13 +0300

    fixed plagin

--- wp-content/plugins/display-movies-devit/display-movies-devit.php
+++ wp-content/plugins/display-movies-devit/display-movies-devit.php
@@ -5,18 +5,17 @@ Description: Display Movies
 Version: 1.0
 Author: Andrey Yeryomin
 */
-
+add_action( 'the_content', 'my_movies_filter' );
 function my_movies_filter($content) {
-  // assuming you have created a page/post entitled 'debug'
-  if ($GLOBALS['post']->post_name == 'movies') {
-   	$params = array(
-	'post_type' => 'movies', // тип постов - записи
-	);
-	$posts = get_posts($params);
-   	var_dump($posts);
-  }
-  // otherwise returns the database content
-  return $content;
+	global $post;
+	if ($post->post_type == 'movies') {
+    	
+    	$bio_box =
+        	"sdsf";
+        return $content . $bio_box;
+    } else {
+        return $content;
+    }
+     
 }
 
-add_filter( 'the_content', 'my_movies_filter' );
